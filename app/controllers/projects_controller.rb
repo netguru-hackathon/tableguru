2dd50dbd89936485560028b7ce7854d8be554447
dalach <wikadalach@gmail.com>
2014-12-05 12:46:28 +0100

    create Project controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:edit, :show, :update, :destroy]
+
+  def index
+    @projects =  Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project), notice: "Project created :)"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to project_path(@project), notice: "Project updated :)"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      redirect_to projects_path, notice: "Project deleted"
+    else
+      redirect_to projects_path, notice: "Couldn't delete the project."
+    end
+  end
+
+  private
+  def set_project
+    @project = Project.find(prams[:project_id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
+end
