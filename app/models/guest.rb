8527e66d6e4e69d097e55103b45861a91a07fccc
Marek Waligórski <marek.waligorski@netguru.pl>
2014-12-05 15:45:27 +0100

    guest doesn't need x,y

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,10 +1,13 @@
 class Guest
   include Mongoid::Document
-  include Positionable
+
+  field :name
+  field :image_url
 
   belongs_to :group
   belongs_to :chair
 
   has_many :friendships
 
+  delegate :x, :y, to: :chair
 end
