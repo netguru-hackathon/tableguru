4cbc659237d7253b758fa3baa956af15d744d723
Marek Waligórski <marek.waligorski@netguru.pl>
2014-12-05 12:47:22 +0100

    devise init

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,4 +23,6 @@ TableGuru::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TableGuru::Application.routes.draw do
 
+  devise_for :users
   root 'home#index'
 
 end
