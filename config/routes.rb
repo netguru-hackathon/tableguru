b46c1fa8b85bcdc7e3990391a20c7404f83aff6f
Marek Waligórski <marek.waligorski@netguru.pl>
2014-12-05 16:34:01 +0100

    index needed too

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ TableGuru::Application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :projects, only: [:index, :create, :update, :show]
-  resources :guests, only: [:show, :create, :update, :destroy]
-  resources :chairs, only: [:show, :create, :update, :destroy]
-  resources :tables, only: [:show, :create, :update, :destroy]
-  resources :friendships, only: [:show, :create, :update, :destroy]
+  resources :guests
+  resources :chairs
+  resources :tables
+  resources :friendships
 end
