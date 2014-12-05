2617dcd85e40c12da90b932951f46ed197f1cd95
Marek Waligórski <marek.waligorski@netguru.pl>
2014-12-05 15:45:14 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ TableGuru::Application.routes.draw do
 
   devise_for :users
   root 'home#index'
-  resources :projects
+  resources :projects, only: [:index, :create, :update, :show]
+  resources :guests, only: [:show, :create, :update, :destroy]
+  resources :chairs, only: [:show, :create, :update, :destroy]
+  resources :tables, only: [:show, :create, :update, :destroy]
+  resources :friendships, only: [:show, :create, :update, :destroy]
 end
